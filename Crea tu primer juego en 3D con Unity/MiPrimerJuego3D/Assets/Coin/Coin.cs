afaed1bb48f24d31b7db4503295605d30e8c9f36
VegetinES <73162998+VegetinES@users.noreply.github.com>
2025-03-17 09:37:21 +0100

    17/03/2025 MiPrimerJuego3D

---
+++ Crea tu primer juego en 3D con Unity/MiPrimerJuego3D/Assets/Coin/Coin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public static int cointCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Coin.cointCount++;
+        Debug.Log("El juego ha comenzado y hay " + Coin.cointCount + " monedas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Estamos en el update");
+    }
+
+    // Cuando el trigger que tiene la moneda, otro objeto entra dentro
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Coin.cointCount--;
+            
+            Debug.Log(other.gameObject.tag + " ha colisionado con " + this.gameObject.name);
+            Debug.Log(other.gameObject.tag + " ha recogido una moneda y ahora quedan " + Coin.cointCount + " monedas");
+
+            if (Coin.cointCount == 0)
+            {
+                Debug.Log("El juego ha terminado");
+                
+                GameObject gameManager = GameObject.Find("GameManager");
+                
+                Destroy(gameManager);
+                
+                GameObject[] fireworksSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+
+                foreach (GameObject firework in fireworksSystem)
+                {
+                    firework.GetComponent<ParticleSystem>().Play();
+                }
+            }
+            
+            Destroy(this.gameObject); //haciendo referencia a que se elimina la moneda
+        }
+        
+        // other.gameObject.name = "Jugador"; // Para establecer un nombre al objeto, en este caso al FPSController, que se llame Jugador
+    }
+}
---
+++ Crea tu primer juego en 3D con Unity/MiPrimerJuego3D/Assets/Coin/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float maxTime = 120f;
+    private float countdown = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Time.deltaTime representa el tiempo en segundos que ha pasado desde el último frame
+        countdown -= Time.deltaTime;
+        
+        Debug.Log(countdown);
+
+        if (countdown <= 0f)
+        {
+            Debug.Log("Se ha terminado el tiempo, has perdido!!!");
+
+            Coin.cointCount = 0;
+            
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
